dc619d4da3f1116e8fd8337895ab741730388ceb
jeffreychengce <jeffreycheng@berkeley.edu>
2025-10-20 19:34:05 -0700

    ls6 script

---
+++ examples/2d_prism/module_frontera.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+ml cuda/12.2
+ml cudnn
+ml nccl
+
+module load intel/19.1.1
+module load impi/19.0.9
+#module load mvapich2-gdr/2.3.7
+#module load mvapich2/2.3.7ss
+
+module load phdf5/1.10.4
+module load python3/3.9.2
+
+export LD_LIBRARY_PATH=/usr/lib64:$LD_LIBRARY_PATH
