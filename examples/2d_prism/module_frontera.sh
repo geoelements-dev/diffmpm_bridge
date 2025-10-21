6cffd1282a2ef222631d594b274b3809e209ea6e
jeffreychengce <jeffreycheng@berkeley.edu>
2025-10-20 19:38:21 -0700

    ls6 script

--- examples/2d_prism/module_frontera.sh
+++ examples/2d_prism/module_frontera.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+
+
+module load intel/19.1.1
+module load impi/19.0.9
+
 ml cuda/12.2
 ml cudnn
 ml nccl
 
-module load intel/19.1.1
-module load impi/19.0.9
 #module load mvapich2-gdr/2.3.7
 #module load mvapich2/2.3.7ss
 
