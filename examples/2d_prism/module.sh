862fdf9ce4a74e8b4d5add41934a74069b0c24d3
jeffreychengce <jeffreycheng@berkeley.edu>
2025-10-23 18:04:34 -0700

    frontera env

--- examples/2d_prism/module.sh
+++ examples/2d_prism/module.sh
@@ -6,8 +6,8 @@ ml nccl
 
 module load intel/19.1.1
 module load impi/19.0.9
-module load mvapich2-gdr/2.3.7
-module load mvapich2/2.3.7
+# module load mvapich2-gdr/2.3.7
+# module load mvapich2/2.3.7
 
 module load phdf5/1.10.4
 module load python3/3.9.2
