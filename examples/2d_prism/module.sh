1d8f45dd1010dccb05606edd7aac630580d05222
jeffreychengce <jeffreycheng@berkeley.edu>
2025-10-23 18:02:56 -0700

    frontera env

--- examples/2d_prism/module.sh
+++ examples/2d_prism/module.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 ml cuda/12.0
-ml cudnn
-ml nccl
+# ml cudnn
+# ml nccl
 
 module load intel/19.1.1
 module load impi/19.0.9
-module load mvapich2-gdr/2.3.7
-module load mvapich2/2.3.7
+# module load mvapich2-gdr/2.3.7
+# module load mvapich2/2.3.7
 
 module load phdf5/1.10.4
 module load python3/3.9.2
